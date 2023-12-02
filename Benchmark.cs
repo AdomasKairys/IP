aa5cab7ede97a3bc269f30c28a72355beea58c38
AdamLeft <80246932+AdamLeft@users.noreply.github.com>
2023-12-02 14:29:42 +0200

    Working on benchmark

---
+++ Benchmark.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.IO;
+
+namespace IP
+{
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        [Params(2, 4, 6, 8, 10, 12)]
+        public int ThredCount { get; set; }
+
+        public (double, double)[][] AllGivPoints { get; set; }
+        public (double, double)[][] AllNewPoints { get; set; }
+
+        [GlobalSetup]
+        public void SetUp()
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                using (var readerx = new StreamReader($"../../../../../../../Data/x{i}.txt"))
+                using (var readery = new StreamReader($"../../../../../../../Data/y{i}.txt"))
+                using (var readerxx = new StreamReader($"../../../../../../../Data/xx{i}.txt"))
+                using (var readeryy = new StreamReader($"../../../../../../../Data/yy{i}.txt"))
+                {
+                    AllGivPoints[i] = readerx.ReadToEnd().Split("\n").Zip(readery.ReadToEnd().Split("\n"), (x, y) => (double.Parse(x.Replace(',','.')), double.Parse(y.Replace(',', '.')))).ToArray();
+                    AllNewPoints[i] = readerxx.ReadToEnd().Split("\n").Zip(readeryy.ReadToEnd().Split("\n"), (x, y) => (double.Parse(x.Replace(',', '.')), double.Parse(y.Replace(',', '.')))).ToArray();
+                }
+            }
+        }
+
+        [Benchmark]
+        public string Test1()
+        {
+            return Optimization.Optimize(AllGivPoints[0], AllNewPoints[0], ThredCount);
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
--- Optimization.cs
+++ Optimization.cs
@@ -3,13 +3,15 @@ using System.Linq;
 using System.Threading;
 using System.Collections.Generic;
 using System.Diagnostics;
+using BenchmarkDotNet.Running;
 
 namespace IP
 {
-    internal class Program
+    internal class Optimization
     {
-        const int THREAD_COUNT = 6; //because of how the program works the ammount of threads created is equal to (THREAD_COUNT/2)^2
-        static double EuclideanNorm((double, double)[] val)
+        private static int THREAD_COUNT = 6; //because of how the program works the ammount of threads created is equal to (THREAD_COUNT/2)^2
+
+        private static double EuclideanNorm((double, double)[] val)
         {
             double sum = 0;
             for(int i = 0; i < val.Length; i++)
@@ -27,7 +29,7 @@ namespace IP
         /// <param name="totalSum">Function that calculates price of point2 in respect to other points</param>
         /// <param name="initialVal">Function that calculates the value/price of point2</param>
         /// <returns>z value</returns>
-        static double Func((double, double)[] points1, (double, double) point2, 
+        private static double Func((double, double)[] points1, (double, double) point2, 
             Func<(double, double), (double, double), double> totalSum, 
             Func<(double, double), double> initialVal)
         {
@@ -37,7 +39,7 @@ namespace IP
         }
 
         //Calculates the sum of values at each new point
-        static double TargetFunc((double, double)[] points1, (double, double)[] points2)
+        private static double TargetFunc((double, double)[] points1, (double, double)[] points2)
         {
             double priceBtwTwoPnts((double, double) p1, (double, double) p2) =>
                             Math.Exp(-0.3 * (Math.Pow(p1.Item1 - p2.Item1, 2) + Math.Pow(p1.Item2 - p2.Item2, 2)));
@@ -57,8 +59,8 @@ namespace IP
             return fullPrice;
         }
 
-        
-        static (double, double)[] JacobianMatrix((double, double)[] points1, (double, double)[] points2)
+
+        private static (double, double)[] JacobianMatrix((double, double)[] points1, (double, double)[] points2)
         {
 
             //partial derivatives
@@ -74,8 +76,6 @@ namespace IP
 
             double dyInit((double, double) p1) =>
                         (4 * Math.Pow(p1.Item2, 3) + 200 * Math.Cos(p1.Item2)) / 1000;
-
-
 
 
             (double, double)[] jac = ((double, double)[])points2.Clone();
@@ -95,8 +95,9 @@ namespace IP
             return jac;
         }
 
-        static void Optimization((double, double)[] pointsGiv, (double, double)[] pointsOpt)
+        public static string Optimize((double, double)[] pointsGiv, (double, double)[] pointsOpt, int threadCount = 6)
         {
+            THREAD_COUNT = threadCount;
             int iter = 0;
             double step = 0.1;
             double eps = 1e-6;
@@ -130,57 +131,81 @@ namespace IP
             {
                 Console.WriteLine(point.Item1 + " " + point.Item2);
             }
+            return "complete";
         }
 
-        static void Main(string[] args)
-        {
-            var r = new Random();
+        //static void Main(string[] args)
+        //{
+        //    var r = new Random();
 
-            //-10+r.NextDouble()*20 == -10+r.NextDouble()*(10-(-10), where 10 and -10 are the ranges of the function and shouldn't be changed
-            (double, double)[] ranInitPoints = Enumerable.Range(0, 1000).Select(i => (-10+r.NextDouble()*20, -10+r.NextDouble()*20)).ToArray();
-            (double, double)[] ranNewPoints = Enumerable.Range(0, 1000).Select(i => (-10+r.NextDouble()*20, -10+r.NextDouble()*20)).ToArray();
+        //    //-10+r.NextDouble()*20 == -10+r.NextDouble()*(10-(-10), where 10 and -10 are the ranges of the function and shouldn't be changed
 
 
-            (double, double)[] initPoints = {(5.42641287, -9.58496101),
-                (2.6729647, 4.97607765),
-                (-0.02985975, -5.50406709),
-                (-6.0387427,  5.21061424),
-                (-6.61778327, - 8.23320372)};
-            (double, double)[] newPoints = { (3.70719637,  9.06786692),
-                 (-9.92103467,  0.24384527),
-                 ( 6.25241923,  2.25052134),
-                 ( 4.43510635, - 4.16247864)};
+        //    (double, double)[] initPoints = {(5.42641287, -9.58496101),
+        //        (2.6729647, 4.97607765),
+        //        (-0.02985975, -5.50406709),
+        //        (-6.0387427,  5.21061424),
+        //        (-6.61778327, - 8.23320372)};
+        //    (double, double)[] newPoints = { (3.70719637,  9.06786692),
+        //         (-9.92103467,  0.24384527),
+        //         ( 6.25241923,  2.25052134),
+        //         ( 4.43510635, - 4.16247864)};
+
+        //    using (var writerx = new StreamWriter("x0.txt"))
+        //    using (var writery = new StreamWriter("y0.txt"))
+        //    using (var writerxx = new StreamWriter("xx0.txt"))
+        //    using (var writeryy = new StreamWriter("yy0.txt"))
+        //    {
+        //        foreach (var point in initPoints)
+        //        {
+        //            writerx.WriteLine(point.Item1);
+        //            writery.WriteLine(point.Item2);
+        //        }
+        //        foreach (var point in newPoints)
+        //        {
+        //            writerxx.WriteLine(point.Item1);
+        //            writeryy.WriteLine(point.Item2);
+        //        }
+        //    }
 
 
-            using (var writerx = new StreamWriter("x.txt"))
-            using (var writery = new StreamWriter("y.txt"))
-            using (var writerxx = new StreamWriter("xx.txt"))
-            using (var writeryy = new StreamWriter("yy.txt"))
-            {
-                foreach (var point in ranInitPoints)
-                {
-                    writerx.WriteLine(point.Item1);
-                    writery.WriteLine(point.Item2);
-                }
-                foreach (var point in ranNewPoints)
-                {
-                    writerxx.WriteLine(point.Item1);
-                    writeryy.WriteLine(point.Item2);
-                }
-            }
+        //    for (int i = 1; i <= 8; i++)
+        //    {
+        //        (double, double)[] ranInitPoints = Enumerable.Range(0, i * i* 15).Select(i => (-10 + r.NextDouble() * 20, -10 + r.NextDouble() * 20)).ToArray();
+        //        (double, double)[] ranNewPoints = Enumerable.Range(0, i * i * 15).Select(i => (-10 + r.NextDouble() * 20, -10 + r.NextDouble() * 20)).ToArray();
+        //        using (var writerx = new StreamWriter($"x{i}.txt"))
+        //        using (var writery = new StreamWriter($"y{i}.txt"))
+        //        using (var writerxx = new StreamWriter($"xx{i}.txt"))
+        //        using (var writeryy = new StreamWriter($"yy{i}.txt"))
+        //        {
+        //            foreach (var point in ranInitPoints)
+        //            {
+        //                writerx.WriteLine(point.Item1);
+        //                writery.WriteLine(point.Item2);
+        //            }
+        //            foreach (var point in ranNewPoints)
+        //            {
+        //                writerxx.WriteLine(point.Item1);
+        //                writeryy.WriteLine(point.Item2);
+        //            }
+        //        }
+        //    }
 
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
+        //    //Stopwatch stopWatch = new Stopwatch();
+        //    //stopWatch.Start();
 
-            Optimization(ranInitPoints, ranNewPoints);
+        //    //Optimize(ranInitPoints, ranNewPoints);
 
-            stopWatch.Stop();
-            TimeSpan ts = stopWatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-            ts.Hours, ts.Minutes, ts.Seconds,
-            ts.Milliseconds / 10);
-            Console.WriteLine("RunTime " + elapsedTime);
+        //    //stopWatch.Stop();
+        //    //TimeSpan ts = stopWatch.Elapsed;
+        //    //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+        //    //ts.Hours, ts.Minutes, ts.Seconds,
+        //    //ts.Milliseconds / 10);
+        //    //Console.WriteLine("RunTime " + elapsedTime);
 
-        }
+
+
+        //    //var summary = BenchmarkRunner.Run<MemoryBenchmarkerDemo>();
+        //}
     }
 }
